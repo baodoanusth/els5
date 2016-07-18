ee6253fcf8f4f8b55fd867d419b2b33b4dd36dc8
baodoanusth <baodoan1131996@gmail.com>
2016-07-18 15:45:24 +0700

    log-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,10 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: Settings.user_password_min}
 
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
